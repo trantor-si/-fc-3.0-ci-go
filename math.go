48bd592badeb2ebe5c57c39dbb384ef8485ea118
trantor-si <luccostajr@gmail.com>
2022-06-13 22:57:31 -0300

    feat: include some operations to math file

--- math.go
+++ math.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(soma(1000, 10))
+	fmt.Println(sum(2, 2))
 }
 
-func soma(a int, b int) int {
+func sum(a int, b int) int {
 	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func times(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
 }
