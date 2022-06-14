a228532ed016e3ae18777a853791c249678f2cc7
trantor-si <luccostajr@gmail.com>
2022-06-13 23:36:42 -0300

    test: Test

--- math_test.go
+++ math_test.go
@@ -14,7 +14,7 @@ func TestSub(t *testing.T) {
 	esperado := 15
 	total := sub(30, 15)
 	if total != esperado {
-		t.Errorf("Resultado da soma de 30-15 é inválido. Resultado %d. Esperado %d", total, esperado)
+		t.Errorf("Resultado da subtração de 30-15 é inválido. Resultado %d. Esperado %d", total, esperado)
 	}
 }
 
@@ -22,13 +22,13 @@ func TestMult(t *testing.T) {
 	esperado := 20
 	total := mult(10, 2)
 	if total != esperado {
-		t.Errorf("Resultado da soma de 10*2 é inválido. Resultado %d. Esperado %d", total, esperado)
+		t.Errorf("Resultado da multiplicação de 10*2 é inválido. Resultado %d. Esperado %d", total, esperado)
 	}
 }
 func TestDiv(t *testing.T) {
 	esperado := 10
 	total := div(20, 2)
 	if total != esperado {
-		t.Errorf("Resultado da soma de 20/2 é inválido. Resultado %d. Esperado %d", total, esperado)
+		t.Errorf("Resultado da divisão de 20/2 é inválido. Resultado %d. Esperado %d", total, esperado)
 	}
 }
---
+++ pub-sonar.bash
@@ -0,0 +1,1 @@
+sonar-scanner -Dsonar.projectKey=go-project -Dsonar.sources=. -Dsonar.host.url=http://localhost:9000 -Dsonar.login=d8890e983503a8987636023d7c0331b6a623b9eeOA
