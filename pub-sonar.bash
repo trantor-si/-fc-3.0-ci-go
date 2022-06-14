4985b06fbd0a8db0dd27809a2a86c5683baba3b0
trantor-si <luccostajr@gmail.com>
2022-06-14 09:01:19 -0300

    test: Test

--- pub-sonar.bash
+++ pub-sonar.bash
@@ -1,1 +1,5 @@
-sonar-scanner -Dsonar.projectKey=go-sonar-project -Dsonar.sources=. -Dsonar.host.url=http://localhost:9000 -Dsonar.login=44da6bbcfd5d554e7de04d732118cfe30617edf1
+sonar-scanner \
+  -Dsonar.projectKey=go-sonar-project \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=9fc6b4e7364020e7319280b443fc9285c3de9bff
