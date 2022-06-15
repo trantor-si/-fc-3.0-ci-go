f1490690f7974e9188e4889da790c040e88d1567
trantor-si <luccostajr@gmail.com>
2022-06-15 00:34:52 -0300

    ci: :sparkles: refactored all sonar-qube and sonar-scanner calls

--- create-sonarqube.bash
+++
@@ -1,1 +0,0 @@
-docker run --name sonarqube -e SONAR_ES_BOOTSTAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:latest
--- pub-sonar.bash
+++
@@ -1,5 +0,0 @@
-sonar-scanner \
-  -Dsonar.projectKey=go-sonar-project \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=9fc6b4e7364020e7319280b443fc9285c3de9bff
